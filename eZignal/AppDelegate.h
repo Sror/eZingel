ce3c9f5839fcda71043bac2ce14dee55db70e3c1
MacMini <MacMini@macmini.home>
2013-05-15 14:31:15 +0200

    Initial Commit

---
+++ eZignal/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  eZignal
+//
+//  Created by MacMini on 5/15/13.
+//  Copyright (c) 2013 MacMini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ eZignal/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  eZignal
+//
+//  Created by MacMini on 5/15/13.
+//  Copyright (c) 2013 MacMini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
